4ab279041cb34c1cbf59d9e56753294f040b5c22
Motaz <motazyusuf@gmail.com>
2024-09-03 22:56:41 +0300

    -BottomNavBar shadow

--- lib/core/theme/application_theme_manager.dart
+++ lib/core/theme/application_theme_manager.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/theme/color_palette.dart';
 
 class ApplicationThemeManager {
-  static ThemeData darkModeTheme = ThemeData();
+  static ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: ColorPalette.backgroudColor,
+    appBarTheme: AppBarTheme(
+      backgroundColor: ColorPalette.appBarColor,
+      iconTheme: IconThemeData(color: ColorPalette.appBarItemsColor),
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+          fontFamily: 'inter',
+          fontSize: 22,
+          fontWeight: FontWeight.w400,
+          color: Colors.white),
+      titleLarge: TextStyle(
+          fontFamily: 'inter',
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          color: Colors.white),
+      titleMedium: TextStyle(
+          fontFamily: 'inter',
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          color: Colors.white),
+      titleSmall: TextStyle(
+          fontFamily: 'inter',
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Colors.white),
+      bodyMedium: TextStyle(
+          fontFamily: 'inter',
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+          color: Color(0xFFCBCBCB)),
+    ),
+  );
 }
