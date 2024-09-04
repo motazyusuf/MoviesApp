7d9994bb2a5f92e3e7ba407f63369dd946b6405d
Motaz <motazyusuf@gmail.com>
2024-09-04 11:20:08 +0300

    -movie_details_view routes

--- lib/core/configurations/app_router.dart
+++ lib/core/configurations/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/configurations/pages_routes.dart';
 
+import '../../views/home/movie_details/movie_details_view.dart';
 import '../../views/main_layout/layout_view.dart';
 import '../../views/splash/splash_view.dart';
 
@@ -14,6 +15,10 @@ class AppRouter {
       case PagesRoutes.layoutView:
         return MaterialPageRoute(
             builder: (context) => LayoutView(), settings: settings);
+
+      case PagesRoutes.movieDetailsView:
+        return MaterialPageRoute(
+            builder: (context) => MovieDetailsView(), settings: settings);
 
       default:
         return MaterialPageRoute(
