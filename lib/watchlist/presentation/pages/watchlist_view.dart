308e2c98b32a7bcb010ca40c6d24f7817facbfc5
Motaz <motazyusuf@gmail.com>
2024-09-12 17:02:30 +0300

    -Watchlist

--- lib/watchlist/presentation/pages/watchlist_view.dart
+++ lib/watchlist/presentation/pages/watchlist_view.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/watchlist/presentation/widgets/vertical_watchlist_item.dart';
 
-class WatchlistView extends StatelessWidget {
+import '../../../core/theme/color_palette.dart';
+
+class WatchlistView extends StatefulWidget {
   const WatchlistView({super.key});
 
   @override
+  State<WatchlistView> createState() => _WatchlistViewState();
+}
+
+class _WatchlistViewState extends State<WatchlistView> {
+  bool isBookmarked = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<Map<String, String>> items = [
+      {"Dora": "assets/images/movieCover.png"},
+      {"Deadpool": "assets/images/movieCover2.png"},
+      {"Narcos": "assets/images/movieCover3.png"},
+    ];
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(100, 75),
+        child: Container(
+          margin: EdgeInsets.only(top: 20),
+          child: AppBar(
+            centerTitle: true,
+            surfaceTintColor: Colors.transparent,
+            title: Text("Watchlist"),
+            backgroundColor: ColorPalette.backgroudColor,
+          ),
+        ),
+      ),
+      body: ListView.separated(
+        scrollDirection: Axis.vertical,
+        separatorBuilder: (context, index) => Divider(
+          height: 20,
+          color: ColorPalette.appBarItemsColor,
+        ),
+        itemBuilder: (context, index) {
+          return VerticalWatchlistItem(
+              index: index, items: items, isBookmarked: isBookmarked);
+        },
+        itemCount: items.length,
+      ),
+    );
   }
 }
---
+++ lib/watchlist/presentation/widgets/vertical_watchlist_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/widgets/vertical_list_item.dart';
+
+class VerticalWatchlistItem extends StatefulWidget {
+  VerticalWatchlistItem(
+      {super.key,
+      required this.items,
+      required this.isBookmarked,
+      required this.index});
+
+  List items;
+  bool isBookmarked;
+  int index;
+
+  @override
+  State<VerticalWatchlistItem> createState() => _VerticalWatchlistItemState();
+}
+
+class _VerticalWatchlistItemState extends State<VerticalWatchlistItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        VerticalListItem(searchResults: widget.items, index: widget.index),
+        InkWell(
+            onTap: () {
+              setState(() {
+                widget.isBookmarked = !widget.isBookmarked;
+              });
+            },
+            child: widget.isBookmarked
+                ? Image.asset("assets/icons/bookmarked.png")
+                : Image.asset(
+                    "assets/icons/add_to_bookmark.png",
+                  ))
+      ],
+    );
+    ;
+  }
+}
