c4ae6a191da7fe0a1cc6ffb67707f331b7d77ae1
Motaz <motazyusuf@gmail.com>
2024-09-16 19:16:58 +0300

    Bloc observer

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String domain = "https://api.themoviedb.org/3/";
+  static const String imageDomain = "https://image.tmdb.org/t/p/w500/";
+  static const String accessToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTVmYzhiNGM2YzkwNzdkNGYwMjE5ZjAyYjQ0YWUzMSIsIm5iZiI6MTcyNjMyNzAzNS4wMTYzNzYsInN1YiI6IjY2ZTVhNTRkZjY3OGViMzUwNjZkYTFiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.58cOlzjGkrQbd1HDo9NOKDG4qHcoQ-LlHIXpJetTImk";
+}
